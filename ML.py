66409a52e75bb6eaf7ce0d50712fe03c09d3e49b
omriatir <88487088+omriatir@users.noreply.github.com>
2023-06-26 16:41:26 +0300

    Update ML.py

--- ML.py
+++ ML.py
@@ -4,11 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from math import log
 
-import tqdm
-# from scipy import interpolate, ndimage
-# from scipy.special import expit
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
-
 # Photometry data file containing 5 columns: Hel.JD, I magnitude, magnitude error, seeing estimation (in pixels - 0.26"/pixel) and sky level.
 df = pd.read_csv('phot.dat', sep=' ', names=['HJD', 'I-magnitude', 'Magnitude Error', 'Seeing Estimation', 'sky level'])
 # Cleaning the data
@@ -24,6 +19,7 @@ df['I/I_star error'] = 10 ** (-(1 / 2.5) * (df['I-magnitude'] - M_star)) * log(1
 time_where_mag_is_max = df['HJD'][argmax(df['I/I_star'])]
 #2019-4-26 9:54:38.02     year-month-day hour:minute:second#
 df['days'] = df['HJD'] - time_where_mag_is_max
+print(time_where_mag_is_max)
 # the form of the fitting that we want to minimize chi_squared in is : a + bx + cx^2
 # the unknown vector is : (a,b,c)
 
@@ -44,39 +40,87 @@ def chi_square_parbola_fit(df):
 
     return a, asig
 
+def process_fit(a, asig):
+    T_0  = -a[1] / (2 * a[0])
+    T_error = np.sqrt((asig[1] / (2 * a[0])) ** 2 + (a[1] * asig[0] / (2 * a[0] ** 2)) ** 2)
+    I_max = a[2] - (a[1] ** 2 / (4 * a[0]))
+    I_max_error = np.sqrt(
+        (asig[2]) ** 2 + (2 * a[1] * asig[1] / (4 * a[0])) ** 2 + (a[1] ** 2 * asig[0] / (4 * a[0] ** 2)) ** 2)
 
-print(chi_square_parbola_fit(smaller_df))
+    return T_0, T_error, I_max, I_max_error
+
+# the initial parameters when running chi squared for real data is:
+umin_theo = 0.919
+umin_theo_error = 0.003
+a , asig = chi_square_parbola_fit(smaller_df)
+initial_params = process_fit(a,asig)
+umin_initial = sqrt(-2+2/np.sqrt(1-1/initial_params[2]**2))
+umin_initial_error = -(sqrt(-2+2/sqrt(1-initial_params[2]**-2))*(1-1/initial_params[2]**2)**1.5 * initial_params[2]**3)**-1 * initial_params[3]
+print(umin_initial_error)
+print(f"{initial_params[0] + time_where_mag_is_max}")
+print(f"{initial_params[1]=}")
+N_sigma = np.abs(umin_initial-umin_theo)/sqrt(umin_theo_error**2 + umin_initial_error**2)
+print("Umin_initial_par is: ")
+print(umin_initial)
+print("N sigma is: ")
+print(N_sigma)
+
+new_x = np.linspace(-25, 25, len(smaller_df))
+predict = a[0]*new_x**2+a[1]*new_x+a[2]
+
+fig, axes = plt.subplots(2, 1, figsize=(6, 9))
+axes[0].errorbar(smaller_df['days'], smaller_df['I/I_star'], yerr=smaller_df['I/I_star error'], fmt='o', ms=3)
+axes[0].plot(new_x, predict, c='red')
+axes[0].grid(1)
+axes[0].title.set_text('Chi Squared Parabolic Fitting')
+axes[0].set(xlabel='$T_0$ [days since 26-4-2019]', ylabel='$I/I_{*}$')
+
+axes[1].errorbar(smaller_df['days'], predict-smaller_df['I/I_star'], yerr=smaller_df['I/I_star error'], fmt='o', ms=3)
+axes[1].axhline(y=0, color='r', linestyle='-', zorder=5)
+axes[1].set(xlabel='$T_0$ [days since 26-4-2019]', ylabel='$F(x_i)-y_i$')
+axes[1].title.set_text('Chi Squared Parabolic Fitting Residuals')
+axes[1].grid(1)
+
+plt.tight_layout(h_pad=3.0)
+plt.show()
+
+# now we use bootstrap method for 10k simulations:
+
+fits = []
+for i in range(10000):
+    # Sample a new dataset
+    data = smaller_df.loc[np.random.choice(smaller_df.index.values, len(smaller_df))]
+    # Fit a new graph
+    a, asig = chi_square_parbola_fit(data)
+    T_0, T_0_error, I_max, I_max_error = process_fit(a, asig)
+    params = [T_0, T_0, I_max, I_max_error]
+    fits.append(params)
+
+fits_df = pd.DataFrame(fits, columns=['T_0', 'T_0_error', 'I_max', 'I_max_error'])
+fits_df['u_min'] = np.sqrt(-2+2/np.sqrt(1-1/fits_df['I_max']**2))
+
+print("The peak of the Umin histogram that we get is: ")
+print(max(fits_df['u_min']))
+print("The peak of the T0 histogram that we get is: ")
+print(max(fits_df['T_0']+ time_where_mag_is_max))
+print("The relative goodness of Umin that we got by the histogram is: ")
+print(np.abs(umin_initial-max(fits_df['u_min']))/umin_initial)
+
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 4))
+
+axes[0].hist(fits_df['T_0'], 50)
+axes[0].grid(1)
+axes[0].title.set_text('$T_0\ Histogram$')
+axes[0].set(xlabel='$T_0$ [days since 26-4-2019]', ylabel='Count')
+
+axes[1].hist(fits_df['u_min'], 50)
+axes[1].grid(1)
+axes[1].title.set_text('$u_{min}\ Histogram$')
+axes[1].set(xlabel='$u_{min}$', ylabel='Count')
+
+plt.show()
 
 
 
-matrix = np.zeros((3,3))
-data_vector = np.array([0,0,0] , dtype=np.int64 )
-
-for j in range(3):
-    for i in range(N):
-        matrix[j][0] = matrix[j][0] + smaller_df['days'][i]**j / smaller_df['I/I_star error'][i]**2
-    for i in range(N):
-        matrix[j][1] = matrix[j][1] + smaller_df['days'][i]**(j+1) / smaller_df['I/I_star error'][i] ** 2
-    for i in range(N):
-        matrix[j][2] = matrix[j][2] + smaller_df['days'][i]**(j+2) / smaller_df['I/I_star error'][i] ** 2
-
-for j in range(3):
-    for i in range(N):
-        data_vector[j] = data_vector[j] + smaller_df['I/I_star'][i] * smaller_df['days'][i]**j / smaller_df['I/I_star error'][i]**2
-
-optimize_params = np.linalg.solve(matrix,data_vector)
-print(optimize_params)
-
-smaller_df.to_csv("results.csv",index=False)
-
-# extracted params :
-T_0 =  optimize_params[1] / (-2 *optimize_params[2])
-I_max = optimize_params[0] - optimize_params[1]**2 / (4*optimize_params[2])
-
-
-# now we start the bootstrap method:
-T_arr = np.zeros(10000)
-u_min_arr = np.zeros(10000)
-
-#for i in range(10000):
 
